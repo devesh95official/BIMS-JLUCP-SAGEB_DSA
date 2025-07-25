a692915b8ce560863540942f111faeda5ab839a9
Devesh Singh <devesh95official@gmail.com>
2025-07-25 12:26:13 +0530

    Update First_Program.cpp

---
+++ 1_Programming fundamental-1/First_Program.cpp
@@ -0,0 +1,15 @@
+
+// Basic Program
+
+#include <iostream> // libraries
+
+using namespace std; // std
+
+
+int main() { // entry point
+
+    //code
+    cout << "HELLO WORLD";
+
+    return 0; // ends the code
+}
